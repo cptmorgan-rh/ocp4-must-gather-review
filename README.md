3fd61710b9c90f5396d3aa6ce1e4dc42c15f03d7
Morgan Peterman <morgan.peterman@redhat.com>
2022-11-18 19:08:07 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,183 @@
+Must Gather Review
+===========================================
+
+DESCRIPTION
+------------
+
+The purpose of this script is to quickly search logs for known issues in an OpenShift must-gather.
+
+PREREQUISITES
+------------
+
+This script requires [yq](https://github.com/kislyuk/yq) and jq. yq can be obtained from the github page or installed by running `pip3 install yq` and the jq package is avaiable in all common Linux Distributions.
+
+CONTRIBUTING
+------------
+
+There are three ways to contribute to this script by either adding a string to an additional function, adding a search of a new pod in the same function/namespace, or adding a new function.
+
+To add an additional string please update the relevant array in the function, e.g. etcd_etcd_errors_arr.
+
+To add an additional search in an existing function please use the following example:
+
+```bash
+# <container_name> pod errors
+<function/namespace_name>_<container_name>_errors_arr=("string_1" "string_2")
+
+for i in namespaces/openshift-<namespace_name>/pods/<podname_*>/<container_name>/<container_name>/logs/current.log; do
+  for val in "${<function/namespace_name>_<container_name>_errors_arr[@]}"; do
+    if [[ "$(grep -wc "$val" "$i")" != "0" ]]; then
+     etcd_output_arr+=("$(echo "$i" | awk -F/ '{ print $2 }')|$(echo "$i" | awk -F/ '{ print $4 }')|$(echo "$val")|$(grep -wc "$val" "$i")")
+    fi
+  done
+done
+```
+
+To add a new function use the following example. If you are having issues with the output please reach out to the repository maintainers for assistance.
+
+```bash
+<function/namespace_name>(){
+
+# set column names
+<function/namespace_name>_output_arr=("NAMESPACE|POD|ERROR|COUNT")
+
+# <container_name> pod errors
+<function/namespace_name>_<container_name>_errors_arr=("string_1" "string_2")
+
+for i in namespaces/openshift-<namespace_name>/pods/<podname_*>/<container_name>/<container_name>/logs/current.log; do
+  for val in "${<function/namespace_name>_<container_name>_errors_arr[@]}"; do
+    if [[ "$(grep -wc "$val" "$i")" != "0" ]]; then
+     etcd_output_arr+=("$(echo "$i" | awk -F/ '{ print $2 }')|$(echo "$i" | awk -F/ '{ print $4 }')|$(echo "$val")|$(grep -wc "$val" "$i")")
+    fi
+  done
+done
+done
+
+printf '%s\n' "${<function/namespace_name>_output_arr[@]}" | column -t -s '|'
+printf "\n"
+
+unset <function/namespace_name>_output_arr
+
+}
+```
+
+INSTALLATION
+------------
+* Copy mg-review.sh to a location inside of your $PATH
+
+USAGE
+------------
+
+```bash
+USAGE: mg-review.sh
+must-gather-review is a simple script which searches a must-gather for known
+issues and reports the namespace, pod, and the count for the errors found.
+
+Options:
+  --events       Displays all events in a given namespace
+  --etcd         Searches for known errors in the etcd pods
+  --kubeapi      Searches for known errors in the kube-apiserver-* pods
+  --scheduler    Searches for known errors in the openshift-kube-scheduler-* pods
+  --dns          Searches for known errors in dns-default-* pods
+  --ingress      Searches for known errors in router-default-* pods
+  --sdn          Searches for known errors in sdn-* pods
+  --kubecontrol  Searches for known errors in kube-controller-manager-* pods
+  --auth         Searches for known errors in oauth-openshift-* pods
+  --csr          Searches for and Prints total Pending CSRs
+  --help         Shows this help message
+```
+
+SAMPLE OUTPUT
+------------
+
+```bash
+$ mg-review.sh --all
+NAMESPACE       POD                      ERROR                                 COUNT
+openshift-etcd  etcd-ocp-85wpv-master-0  took too long                         2331
+openshift-etcd  etcd-ocp-85wpv-master-0  local node might have slow network    6
+openshift-etcd  etcd-ocp-85wpv-master-0  elected leader                        1
+openshift-etcd  etcd-ocp-85wpv-master-0  lost leader                           1
+openshift-etcd  etcd-ocp-85wpv-master-1  took too long                         14177
+openshift-etcd  etcd-ocp-85wpv-master-1  local node might have slow network    14
+openshift-etcd  etcd-ocp-85wpv-master-1  elected leader                        7
+openshift-etcd  etcd-ocp-85wpv-master-1  lost leader                           6
+openshift-etcd  etcd-ocp-85wpv-master-1  failed to send out heartbeat on time  4310
+openshift-etcd  etcd-ocp-85wpv-master-2  took too long                         10008
+openshift-etcd  etcd-ocp-85wpv-master-2  local node might have slow network    8
+openshift-etcd  etcd-ocp-85wpv-master-2  elected leader                        9
+openshift-etcd  etcd-ocp-85wpv-master-2  lost leader                           8
+openshift-etcd  etcd-ocp-85wpv-master-2  failed to send out heartbeat on time  484
+
+Stats about last 500 etcd 'took long' messages: etcd-ocp4-85wpv-master-0
+	Max: 1393.604461ms
+	Min: 200.102143ms
+	Avg: 361ms
+	Expected: 200ms
+
+Stats about last 500 etcd 'took long' messages: etcd-ocp4-85wpv-master-1
+	Max: 1402.816156ms
+	Min: 200.220565ms
+	Avg: 327ms
+	Expected: 200ms
+
+Stats about last 500 etcd 'took long' messages: etcd-ocp4-85wpv-master-2
+	Max: 5749.69575ms
+	Min: 200.196156ms
+	Avg: 572ms
+	Expected: 200ms
+
+Last 500 etcd DB Compaction times: etcd-ocp4-85wpv-master-0
+	Max: 453.066099ms
+	Min: 165.595496ms
+	Avg: 182ms
+
+Last 500 etcd DB Compaction times: etcd-ocp4-85wpv-master-1
+	Max: 548.300888ms
+	Min: 164.605164ms
+	Avg: 182ms
+
+Last 500 etcd DB Compaction times: etcd-ocp4-85wpv-master-2
+	Max: 453.216023ms
+	Min: 166.309408ms
+	Avg: 183ms
+
+NAMESPACE                 POD                                ERROR                            COUNT
+openshift-kube-apiserver  kube-apiserver-ocp4-85wpv-master-0  timeout or abort while handling  1
+openshift-kube-apiserver  kube-apiserver-ocp4-85wpv-master-1  timeout or abort while handling  264
+openshift-kube-apiserver  kube-apiserver-ocp4-85wpv-master-2  timeout or abort while handling  30
+
+NAMESPACE                 POD                                          ERROR                                                                        COUNT
+openshift-kube-scheduler  openshift-kube-scheduler-ocp4-85wpv-master-2  net/http: request canceled (Client.Timeout exceeded while awaiting headers)  1
+
+NAMESPACE      POD                ERROR                   COUNT
+openshift-dns  dns-default-66kzc  i/o timeout             171
+openshift-dns  dns-default-66kzc  client connection lost  11
+openshift-dns  dns-default-8s8md  i/o timeout             7119
+openshift-dns  dns-default-gtzbq  i/o timeout             6201
+openshift-dns  dns-default-gtzbq  client connection lost  3
+openshift-dns  dns-default-lns6x  i/o timeout             20
+openshift-dns  dns-default-q6wql  i/o timeout             1
+openshift-dns  dns-default-q6wql  client connection lost  3
+openshift-dns  dns-default-s6pnv  client connection lost  3
+
+NAMESPACE          POD                              ERROR                                         COUNT
+openshift-ingress  router-default-85fdd489f9-mnw5w  unable to find service                        2
+openshift-ingress  router-default-85fdd489f9-mnw5w  Failed to make webhook authenticator request  3
+openshift-ingress  router-default-85fdd489f9-zwjlm  unable to find service                        2
+openshift-ingress  router-default-85fdd489f9-zwjlm  Failed to make webhook authenticator request  1
+
+NAMESPACE                 POD                               ERROR                                                 COUNT
+openshift-authentication  oauth-openshift-85b74fff74-fxrbb  the server is currently unable to handle the request  1
+openshift-authentication  oauth-openshift-85b74fff74-gwrzn  the server is currently unable to handle the request  2
+openshift-authentication  oauth-openshift-85b74fff74-gwrzn  Client.Timeout exceeded while awaiting headers        1
+openshift-authentication  oauth-openshift-85b74fff74-zqgq5  the server is currently unable to handle the request  1
+
+NAMESPACE                          POD                                         ERROR                                                 COUNT
+openshift-kube-controller-manager  kube-controller-manager-ocp-85wpv-master-2  the server is currently unable to handle the request  104
+
+Total Unsigned CSRs: 49
+```
+
+AUTHOR
+------
+Morgan Peterman
---
+++ mg-review.sh
@@ -0,0 +1,491 @@
+#!/bin/bash
+
+run() {
+
+  case "$1" in
+    --all)
+      all
+      ;;
+    --events)
+      events $2
+      ;;
+    --auth)
+      auth
+      ;;
+    --etcd)
+      etcd
+      ;;
+    --kubeapi)
+      kube-api
+      ;;
+    --scheduler)
+      scheduler
+      ;;
+    --dns)
+      dns
+      ;;
+    --ingress)
+      ingress
+      ;;
+    --sdn)
+      sdn
+      ;;
+    --kubecontrol)
+      kube_controller
+      ;;
+    --csr)
+      csr
+      ;;
+    --help)
+      show_help
+      ;;
+    *)
+      show_help
+      exit 0
+  esac
+
+}
+
+show_help(){
+
+cat  << ENDHELP
+USAGE: $(basename "$0")
+must-gather-review is a simple script which searches a must-gather for known
+issues and reports the namespace, pod, and the count for the errors found.
+
+Options:
+  --events       Displays all events in a given namespace
+  --etcd         Searches for known errors in the etcd pods
+  --kubeapi      Searches for known errors in the kube-apiserver-* pods
+  --scheduler    Searches for known errors in the openshift-kube-scheduler-* pods
+  --dns          Searches for known errors in dns-default-* pods
+  --ingress      Searches for known errors in router-default-* pods
+  --sdn          Searches for known errors in sdn-* pods
+  --kubecontrol  Searches for known errors in kube-controller-manager-* pods
+  --auth         Searches for known errors in oauth-openshift-* pods
+  --csr          Searches for and Prints total Pending CSRs
+  --help         Shows this help message
+
+ENDHELP
+
+}
+
+dep_check(){
+
+if [ ! $(command -v jq) ]; then
+  echo "jq not found. Please install jq."
+  exit 1
+fi
+
+if [ ! $(command -v yq) ]; then
+  echo "yq not found. Please install yq by running pip3 install yq"
+  exit 1
+fi
+
+}
+
+all(){
+
+  etcd
+
+  kube-api
+
+  scheduler
+
+  dns
+
+  ingress
+
+  sdn
+
+  auth
+
+  kube_controller
+
+  csr
+
+}
+
+events(){
+
+if [ -f "namespaces/$1/core/events.yaml" ]; then
+  events_arr=("NODE|POD|TYPE|REASON|MESSAGE")
+  for i in $(yq . namespaces/$1/core/events.yaml | jq -r '.items | keys[]'); do
+    events_arr+=($(yq -r . namespaces/$1/core/events.yaml | jq -r "[(select(.items[$i] | select(.type == \"Warning\")) | .items[$i].source.host), (select(.items[$i] | select(.type == \"Warning\")) | .items[$i].involvedObject.name), (select(.items[$i] | select(.type == \"Warning\")) | .items[$i].type), (select(.items[$i] | select(.type == \"Warning\")) | .items[$i].reason), (select(.items[$i] | select(.type == \"Warning\")) | .items[$i].message)] | join (\"|\")" | sed 's/ /_/g'))
+  done
+else
+  echo "Events missing for namespace: $1"
+  exit 1
+fi
+
+if [ "${#events_arr[1]}" != 0 ]; then
+  printf '%s\n' "${events_arr[@]}" | column -t -s '|'
+  printf "\n"
+else
+  echo "No warnings found in namespace: $1"
+  exit
+fi
+
+
+unset events_arr
+
+}
+
+etcd(){
+
+#Check to make sure the openshift-etcd namespace exits
+if [ ! -d "namespaces/openshift-etcd/pods/" ]; then
+  echo -e "openshift-etcd not found.\n"
+  return 1
+fi
+
+# set column names
+etcd_output_arr=("NAMESPACE|POD|ERROR|COUNT")
+
+# etcd pod errors
+etcd_etcd_errors_arr=("slow fdatasync" "took too long" "local node might have slow network" "elected leader" "lost leader" "wal: sync duration" "the clock difference against peer" "lease not found" "rafthttp: failed to read" "server is likely overloaded" "failed to send out heartbeat on time" "lost the tcp streaming" "sending buffer is full")
+
+for i in namespaces/openshift-etcd/pods/etcd*/etcd/etcd/logs/current.log; do
+  for val in "${etcd_etcd_errors_arr[@]}"; do
+    if [[ "$(grep -wc "$val" "$i")" != "0" ]]; then
+     etcd_output_arr+=("$(echo "$i" | awk -F/ '{ print $2 }')|$(echo "$i" | awk -F/ '{ print $4 }')|$(echo "$val")|$(grep -wc "$val" "$i")")
+    fi
+  done
+done
+
+
+if [ "${#etcd_output_arr[1]}" != 0 ]; then
+  printf '%s\n' "${etcd_output_arr[@]}" | column -t -s '|'
+  printf "\n"
+fi
+
+unset etcd_output_arr
+
+for i in namespaces/openshift-etcd/pods/etcd*/etcd/etcd/logs/current.log; do
+    max=0
+    min=9999
+    avg=0
+    count=0
+    expected=$(grep -m1 'took too long.*expec' "$i" | cut -d' ' -f2- | jq -r '."expected-duration"' 2>/dev/null)
+    if grep 'took too long.*expec' "$i" > /dev/null 2>&1;
+    then
+      for x in $(grep 'took too long.*expec' "$i" | grep -v leader | tail -n 500 | cut -d' ' -f2- | jq -r '.took' 2>/dev/null); do
+        if [[ $x =~ [1-9]s ]];
+        then
+         compact_time=$(echo "scale=2;$(echo $x | sed 's/s//')*1000" | bc)
+        else
+          compact_time=$(echo $x | sed 's/ms//')
+        fi
+        if [[ $(echo "$compact_time > $max" | bc -l 2>/dev/null) -eq 1 ]];
+        then
+          max=$(echo $compact_time | sed -e 's/[0]*$//g')
+        fi
+        if [[ $(echo "$compact_time > $min" | bc -l 2>/dev/null) -eq 0 ]];
+        then
+          min=$(echo $compact_time | sed -e 's/[0]*$//g')
+        fi
+        count=$(( $count + 1 ))
+        avg=$(echo "$avg + $compact_time" | bc )
+      done
+      printf "Stats about last 500 etcd 'took long' messages: $(echo "$i" | awk -F/ '{ print $4 }')\n"
+      printf "\tMax: ${max}ms\n"
+      printf "\tMin: ${min}ms\n"
+      printf "\tAvg: $(echo "$avg/$count" | bc)ms\n"
+      printf "\tExpected: ${expected}\n"
+      printf "\n"
+    fi
+done
+
+for i in namespaces/openshift-etcd/pods/etcd*/etcd/etcd/logs/current.log; do
+    max=0
+    min=9999
+    avg=0
+    count=0
+    if grep -m1 "finished scheduled compaction" "$i"  > /dev/null 2>&1;
+    then
+      for x in $(grep "finished scheduled compaction" "$i" | tail -n 500 | cut -d' ' -f2- | sed 's/\\/\\\\/g' | jq -r '.took'); do
+        if [[ $x =~ [1-9]s ]];
+        then
+          compact_time=$(echo "scale=2;$(echo $x | sed 's/s//')*1000" | bc)
+        else
+          compact_time=$(echo $x | sed 's/ms//')
+        fi
+        if [[ $(echo "$compact_time > $max" | bc -l) -eq 1 ]];
+        then
+          max=$(echo $compact_time | sed -e 's/[0]*$//g')
+        fi
+        if [[ $(echo "$compact_time > $min" | bc -l) -eq 0 ]];
+        then
+          min=$(echo $compact_time | sed -e 's/[0]*$//g')
+        fi
+        count=$(( $count + 1 ))
+        avg=$(echo "$avg + $compact_time" | bc )
+      done
+      printf "Last 500 etcd DB Compaction times: $(echo "$i" | awk -F/ '{ print $4 }')\n"
+      printf "\tMax: ${max}ms\n"
+      printf "\tMin: ${min}ms\n"
+      printf "\tAvg: $(echo "$avg/$count" | bc)ms\n"
+      printf "\n"
+    fi
+done
+
+}
+
+kube-api(){
+
+#Check to make sure the openshift-kube-apiserver namespace exits
+if [ ! -d "namespaces/openshift-kube-apiserver/pods/" ]; then
+  echo -e "openshift-kube-apiserver not found.\n"
+  return 1
+fi
+
+# set column names
+kubeapi_output_arr=("NAMESPACE|POD|ERROR|COUNT")
+
+# kube-apiserver pod errors
+kubeapi_errors_arr=("timeout or abort while handling")
+
+for i in namespaces/openshift-kube-apiserver/pods/kube-apiserver-*/kube-apiserver/kube-apiserver/logs/current.log; do
+  for val in "${kubeapi_errors_arr[@]}"; do
+    if [[ "$(grep -wc "$val" "$i")" != "0" ]]; then
+     kubeapi_output_arr+=("$(echo "$i" | awk -F/ '{ print $2 }')|$(echo "$i" | awk -F/ '{ print $4 }')|$(echo "$val")|$(grep -wc "$val" "$i")")
+    fi
+  done
+done
+
+if [ "${#kubeapi_output_arr[1]}" != 0 ]; then
+  printf '%s\n' "${kubeapi_output_arr[@]}" | column -t -s '|'
+  printf "\n"
+fi
+
+unset kubeapi_output_arr
+
+}
+
+scheduler(){
+
+#Check to make sure the openshift-kube-scheduler namespace exits
+if [ ! -d "namespaces/openshift-kube-scheduler/pods/" ]; then
+  echo -e "openshift-kube-scheduler not found.\n"
+  return 1
+fi
+
+# set column names
+scheduler_output_arr=("NAMESPACE|POD|ERROR|COUNT")
+
+# kube-scheduler pod errors
+scheduler_errors_arr=("net/http: request canceled (Client.Timeout exceeded while awaiting headers)" "6443: connect: connection refused" "Failed to update lock: etcdserver: request timed out")
+
+for i in namespaces/openshift-kube-scheduler/pods/openshift-kube-scheduler-*/kube-scheduler/kube-scheduler/logs/current.log; do
+  for val in "${scheduler_errors_arr[@]}"; do
+    if [[ "$(grep -wc "$val" "$i")" != "0" ]]; then
+     scheduler_output_arr+=("$(echo "$i" | awk -F/ '{ print $2 }')|$(echo "$i" | awk -F/ '{ print $4 }')|$(echo "$val")|$(grep -wc "$val" "$i")")
+    fi
+  done
+done
+
+if [ "${#scheduler_output_arr[1]}" != 0 ]; then
+  printf '%s\n' "${scheduler_output_arr[@]}" | column -t -s '|'
+  printf "\n"
+fi
+
+unset scheduler_output_arr
+
+}
+
+dns(){
+
+#Check to make sure the openshift-dns namespace exits
+if [ ! -d "namespaces/openshift-dns/pods/" ]; then
+  echo -e "openshift-dns not found.\n"
+  return 1
+fi
+
+# set column names
+dns_output_arr=("NAMESPACE|POD|ERROR|COUNT")
+
+# dns pod errors
+dns_errors_arr=("TLS handshake timeout" "i/o timeout" "connection reset by peer" "client connection lost" "no route to host" "connection refused")
+
+for i in namespaces/openshift-dns/pods/dns-default-*/dns/dns/logs/current.log; do
+  for val in "${dns_errors_arr[@]}"; do
+    if [[ "$(grep -wc "$val" "$i")" != "0" ]]; then
+     dns_output_arr+=("$(echo "$i" | awk -F/ '{ print $2 }')|$(echo "$i" | awk -F/ '{ print $4 }')|$(echo "$val")|$(grep -wc "$val" "$i")")
+    fi
+  done
+done
+
+if [ "${#dns_output_arr[1]}" != 0 ]; then
+  printf '%s\n' "${dns_output_arr[@]}" | column -t -s '|'
+  printf "\n"
+fi
+
+unset dns_output_arr
+
+}
+
+ingress(){
+
+#Check to make sure the openshift-ingress namespace exits
+if [ ! -d "namespaces/openshift-ingress/pods/" ]; then
+  echo -e "openshift-ingress not found.\n"
+  return 1
+fi
+
+# set column names
+ingress_output_arr=("NAMESPACE|POD|ERROR|COUNT")
+
+# router pod errors
+ingress_errors_arr=("unable to find service" "error reloading router: exit status 1" "connection refused" "Failed to make webhook authenticator request")
+
+for i in namespaces/openshift-ingress/pods/router-default-*/router/router/logs/current.log; do
+  for val in "${ingress_errors_arr[@]}"; do
+    if [[ "$(grep -wc "$val" "$i")" != "0" ]]; then
+     ingress_output_arr+=("$(echo "$i" | awk -F/ '{ print $2 }')|$(echo "$i" | awk -F/ '{ print $4 }')|$(echo "$val")|$(grep -wc "$val" "$i")")
+    fi
+  done
+done
+
+if [ "${#ingress_output_arr[1]}" != 0 ]; then
+  printf '%s\n' "${ingress_output_arr[@]}" | column -t -s '|'
+  printf "\n"
+fi
+
+unset ingress_output_arr
+
+}
+
+sdn(){
+
+#Check to make sure the openshift-sdn namespace exits
+if [ ! -d "namespaces/openshift-sdn/pods/" ]; then
+  echo -e "openshift-sdn not found.\n"
+  return 1
+fi
+
+# set column names
+sdn_output_arr=("NAMESPACE|POD|ERROR|COUNT")
+
+# sdn pod errors
+sdn_errors_arr=("connection refused" "an error on the server (\"\") has prevented the request from succeeding" "Failed to get local addresses during proxy sync" "the server has received too many requests and has asked us to try again later")
+
+for i in namespaces/openshift-sdn/pods/sdn-*/sdn/sdn/logs/current.log; do
+  for val in "${sdn_errors_arr[@]}"; do
+    if [[ "$(grep -wc "$val" "$i")" != "0" ]]; then
+     sdn_output_arr+=("$(echo "$i" | awk -F/ '{ print $2 }')|$(echo "$i" | awk -F/ '{ print $4 }')|$(echo "$val")|$(grep -wc "$val" "$i")")
+    fi
+  done
+done
+
+if [ "${#sdn_output_arr[1]}" != 0 ]; then
+  printf '%s\n' "${sdn_output_arr[@]}" | column -t -s '|'
+  printf "\n"
+fi
+
+unset sdn_output_arr
+
+}
+
+auth(){
+
+#Check to make sure the openshift-authentication namespace exits
+if [ ! -d "namespaces/openshift-authentication/pods/" ]; then
+  echo -e "openshift-authentication not found.\n"
+  return 1
+fi
+
+# set column names
+auth_output_arr=("NAMESPACE|POD|ERROR|COUNT")
+
+# oauth-openshift pod errors
+auth_errors_arr=("the server is currently unable to handle the request" "Client.Timeout exceeded while awaiting headers")
+
+for i in namespaces/openshift-authentication/pods/oauth-openshift-*/oauth-openshift/oauth-openshift/logs/current.log; do
+  for val in "${auth_errors_arr[@]}"; do
+    if [[ "$(grep -wc "$val" "$i")" != "0" ]]; then
+     auth_output_arr+=("$(echo "$i" | awk -F/ '{ print $2 }')|$(echo "$i" | awk -F/ '{ print $4 }')|$(echo "$val")|$(grep -wc "$val" "$i")")
+    fi
+  done
+done
+
+if [ "${#auth_output_arr[1]}" != 0 ]; then
+  printf '%s\n' "${auth_output_arr[@]}" | column -t -s '|'
+  printf "\n"
+fi
+
+unset auth_output_arr
+
+}
+
+kube_controller(){
+
+#Check to make sure the openshift-authekube-controller-managerntication namespace exits
+if [ ! -d "namespaces/openshift-kube-controller-manager/pods/" ]; then
+  echo -e "openshift-kube-controller-manager not found.\n\n"
+  return 1
+fi
+
+# set column names
+kube_controller_output_arr=("NAMESPACE|POD|ERROR|COUNT")
+
+# kube-controller-manager pod errors
+kube_controller_errors_arr=("the server is currently unable to handle the request")
+
+for i in namespaces/openshift-kube-controller-manager/pods/kube-controller-manager-*/kube-controller-manager/kube-controller-manager/logs/current.log; do
+  for val in "${kube_controller_errors_arr[@]}"; do
+    if [[ "$(grep -wc "$val" "$i")" != "0" ]]; then
+     kube_controller_output_arr+=("$(echo "$i" | awk -F/ '{ print $2 }')|$(echo "$i" | awk -F/ '{ print $4 }')|$(echo "$val")|$(grep -wc "$val" "$i")")
+    fi
+  done
+done
+
+if [ "${#kube_controller_output_arr[1]}" != 0 ]; then
+  printf '%s\n' "${kube_controller_output_arr[@]}" | column -t -s '|'
+  printf "\n"
+fi
+
+unset kube_controller_output_arr
+
+}
+
+csr(){
+
+#Check to make sure the openshift-authekube-controller-managerntication namespace exits
+if [ ! -d  cluster-scoped-resources/certificates.k8s.io/certificatesigningrequests/ ]; then
+  echo -e "No CSRs found.\n\n"
+  return 1
+fi
+
+# set column names
+csr_arr=("NAME|CREATED_ON|REQUESTOR")
+
+for i in $(ls cluster-scoped-resources/certificates.k8s.io/certificatesigningrequests/*.yaml); do
+  csr_arr+=("$(yq . "$i" | jq -r 2>/dev/null '[select(.status == {}) | .metadata.name, .metadata.creationTimestamp, .spec.username] | join("|")')")
+done
+
+#Subtract one line for true count
+csr_total=$(( ${#csr_arr[@]} - 1 ))
+printf "Total Unsigned CSRs: ${csr_total}\n"
+
+}
+
+main(){
+
+#Check if in must-gather folder
+if [ ! -d namespaces ]
+then
+    printf "WARNING: Namespaces not found.\n"
+    printf "Please run $(basename "$0") from inside a must-gather folder.\n"
+    printf "\n"
+    show_help
+    exit 1
+fi
+
+#Verify yq and jq are installed
+dep_check
+
+run "$1" "$2"
+
+}
+
+main "$@"
